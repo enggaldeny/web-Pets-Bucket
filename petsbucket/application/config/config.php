ea31511fb535b1bb0ba5b399d003bf66c5343e64
eng-PC\eng <rm.enggal33@gmail.com>
2017-05-17 14:59:07 +0700

    terbaru lagi

--- petsbucket/application/config/config.php
+++ petsbucket/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kuliah/web_base/petsbucket/';
+$config['base_url'] = 'http://localhost/kuliah/web-base/petsbucket/';
 
 /*
 |--------------------------------------------------------------------------
